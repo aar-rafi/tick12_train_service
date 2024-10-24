8ff5793eeb5d552195f4f3c83f22cd4b85b47aa6
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-24 20:09:25 +0600

    endpoint changed from / to /get

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_train_names_view
 
 urlpatterns = [
-    path('', get_train_names_view, name='get_train_names'),
+    path('get/', get_train_names_view, name='get_train_names'),
     # Other URLs
 ]
 
