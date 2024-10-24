1fcbcc800f09d951afdba0d1343495ed8a1f509d
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-25 05:14:32 +0600

    moved cors middleware up

--- project/settings.py
+++ project/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django_prometheus.middleware.PrometheusBeforeMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
