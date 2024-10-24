733ea9f2ab1d22b51d67db20911e76005db16394
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-25 01:43:27 +0600

    removed extra slash from endpoint

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_train_names_view
 
 urlpatterns = [
-    path('api/trains/get/', get_train_names_view, name='get_train_names'),
+    path('api/trains/get', get_train_names_view, name='get_train_names'),
     # Other URLs
 ]
 
